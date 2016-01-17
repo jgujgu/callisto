a479fa0f70b824e625ce5a4b42fd415ebd0a0324
jgu <jgu@jeffreygu.com>
2016-01-16 18:59:54 -0700

    added location info to placeholder and moved geocoding action to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :geocode_user
+
+  def geocode_user
+    if Rails.env.development? || Rails.env.test?
+      @ip_address = "75.148.122.29"
+    else
+      @ip_address = request.remote_ip
+    end
+    @geocoder_result = Geocoder.search(@ip_address).first.data
+  end
 end
--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -4,12 +4,6 @@ module Spree
     respond_to :html
 
     def index
-      if Rails.env.development? || Rails.env.test?
-        @ip_address = "75.148.122.29"
-      else
-        @ip_address = request.remote_ip
-      end
-      @geocoder_result = Geocoder.search(@ip_address).first.data
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products
       @taxonomies = Spree::Taxonomy.includes(root: :children)
