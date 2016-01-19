f80a79ffd2a3168edd52dfe2e8c7724c1a263b14
jgu <jgu@jeffreygu.com>
2016-01-18 19:32:12 -0700

    add hide inside phpone

--- app/assets/javascripts/spree/frontend/scroll.js
+++ app/assets/javascripts/spree/frontend/scroll.js
@@ -1,25 +1,25 @@
 var lastScrollTop = 0;
 var md = new MobileDetect(window.navigator.userAgent);
 var searchPlaceholder;
-$(document).ready(function() {
-  var $navSearchForm = $("#nav-search-form");
-  var $btnSearch = $("#btn-search");
-  var $keywords = $("#keywords");
-  searchPlaceholder = $keywords.attr("placeholder");
-  $keywords.bind('focusin focus', function(e){
-    e.preventDefault();
-    $navSearchForm.removeClass("shrunken-search");
-    showSearch($navSearchForm, $btnSearch, $keywords);
-  });
-  setInterval(function() {
-    if (!$keywords.is(':focus')) {
-      hideSearch($navSearchForm, $btnSearch, $keywords);
-    }
-  }, 6000);
-});
 
 
 if (md.phone()) {
+  $(document).ready(function() {
+    var $navSearchForm = $("#nav-search-form");
+    var $btnSearch = $("#btn-search");
+    var $keywords = $("#keywords");
+    searchPlaceholder = $keywords.attr("placeholder");
+    $keywords.bind('focusin focus', function(e){
+      e.preventDefault();
+      $navSearchForm.removeClass("shrunken-search");
+      showSearch($navSearchForm, $btnSearch, $keywords);
+    });
+    setInterval(function() {
+      if (!$keywords.is(':focus')) {
+        hideSearch($navSearchForm, $btnSearch, $keywords);
+      }
+    }, 6000);
+  });
   $(window).scroll(function(event){
     var st = $(this).scrollTop();
     var $navSearchForm = $("#nav-search-form");
