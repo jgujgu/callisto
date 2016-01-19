f550b1f756ec77122ded413112d7ec1ace05fe82
jgu <jgu@jeffreygu.com>
2016-01-18 17:34:24 -0700

    solution im happiest with

--- app/assets/javascripts/spree/frontend/scroll.js
+++ app/assets/javascripts/spree/frontend/scroll.js
@@ -1,17 +1,41 @@
 var lastScrollTop = 0;
 var md = new MobileDetect(window.navigator.userAgent);
+var searchPlaceholder;
+$(document).ready(function() {
+    searchPlaceholder = $("#keywords").attr("placeholder");
+    console.log(searchPlaceholder);
+});
 
 if (md.phone()) {
   $(window).scroll(function(event){
     var st = $(this).scrollTop();
-    $navSearchForm = $(".nav-search-form");
+    var $navSearchForm = $("#nav-search-form");
+    var $btnSearch = $("#btn-search");
+    var $keywords = $("#keywords");
+    if (st === 0) {
+      return;
+    }
     if (st > lastScrollTop){
-      $navSearchForm.addClass("shrunken-search");
-      console.log("down");
+      hideSearch($navSearchForm, $btnSearch, $keywords);
     } else {
-      console.log("up");
       $navSearchForm.removeClass("shrunken-search");
+      setTimeout(function() {
+        showSearch($navSearchForm, $btnSearch, $keywords);
+      },500);
     }
     lastScrollTop = st;
   });
+}
+
+function hideSearch($navSearchForm, $btnSearch, $keywords) {
+  $navSearchForm.addClass("shrunken-search");
+  $btnSearch.addClass("transparent");
+  $keywords.addClass("transparent");
+  $keywords.attr("placeholder", "");
+}
+
+function showSearch($navSearchForm, $btnSearch, $keywords) {
+  $btnSearch.removeClass("transparent");
+  $keywords.removeClass("transparent");
+  $keywords.attr("placeholder", searchPlaceholder);
 }
