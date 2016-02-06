b4fcbf93a8502546cd22f782ee0d7ce7fad19326
jgu <jgu@jeffreygu.com>
2016-02-06 16:40:57 -0700

    implemented hover overlays

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -6,10 +6,13 @@ module Spree
     def index
       subdomain = request.subdomain
       if subdomain.empty? || subdomain == "www" || subdomain == "flea"
+        @searcher = build_searcher(params.merge(include_images: true))
+        @products = @searcher.retrieve_products
+        @taxonomies = Spree::Taxonomy.includes(root: :children)
         @stores = Spree::Store.showcases
 
         products = @stores.map do |s|
-          s.products.showcases
+          s.products.showcases.limit(4)
         end
 
         @product_batches = (0..3).map do |i|
@@ -19,18 +22,18 @@ module Spree
         end
 
         #user_marker = Gmaps4rails.build_markers([@geocoder_result]) do |gr, marker|
-          #marker.lat gr["latitude"]
-          #marker.lng gr["longitude"]
-          #marker.infowindow gr["ip"]
+        #marker.lat gr["latitude"]
+        #marker.lng gr["longitude"]
+        #marker.infowindow gr["ip"]
         #end
 
         #index = 0
         #store_markers = Gmaps4rails.build_markers(@stores) do |store, marker|
-          #marker.lat store[:latitude]
-          #marker.lng store[:longitude]
-          #marker.infowindow render_to_string(:partial => "/spree/partials/store_marker", locals: { store: store, index: index})
-          #marker.json({ id: index })
-          #index += 1
+        #marker.lat store[:latitude]
+        #marker.lng store[:longitude]
+        #marker.infowindow render_to_string(:partial => "/spree/partials/store_marker", locals: { store: store, index: index})
+        #marker.json({ id: index })
+        #index += 1
         #end
         #@markers = store_markers + user_marker
       else
