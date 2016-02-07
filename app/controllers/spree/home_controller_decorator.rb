0304fe67d8c32d931f51d101f81e1ee27ac8fe95
jgu <jgu@jeffreygu.com>
2016-02-06 23:34:46 -0700

    routed links correctly

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -21,21 +21,21 @@ module Spree
           end
         end
 
-        #user_marker = Gmaps4rails.build_markers([@geocoder_result]) do |gr, marker|
-        #marker.lat gr["latitude"]
-        #marker.lng gr["longitude"]
-        #marker.infowindow gr["ip"]
-        #end
+        user_marker = Gmaps4rails.build_markers([@geocoder_result]) do |gr, marker|
+          marker.lat gr["latitude"]
+          marker.lng gr["longitude"]
+          marker.infowindow gr["ip"]
+        end
 
-        #index = 0
-        #store_markers = Gmaps4rails.build_markers(@stores) do |store, marker|
-        #marker.lat store[:latitude]
-        #marker.lng store[:longitude]
-        #marker.infowindow render_to_string(:partial => "/spree/partials/store_marker", locals: { store: store, index: index})
-        #marker.json({ id: index })
-        #index += 1
-        #end
-        #@markers = store_markers + user_marker
+        index = 0
+        store_markers = Gmaps4rails.build_markers(@stores) do |store, marker|
+          marker.lat store[:latitude]
+          marker.lng store[:longitude]
+          marker.infowindow render_to_string(:partial => "/spree/partials/store_marker", locals: { store: store, index: index})
+          marker.json({ id: index })
+          index += 1
+        end
+        @markers = store_markers + user_marker
       else
         store = Spree::Store.find_by(code: subdomain)
         if store
