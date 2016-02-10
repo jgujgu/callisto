8d36852aa99a97b5312274f97a32dc54a1cd3afb
jgu <jgu@jeffreygu.com>
2016-02-09 22:23:52 -0600

    change select arrow

--- app/overrides/use_drop_down_for_variants.rb
+++ app/overrides/use_drop_down_for_variants.rb
@@ -2,7 +2,11 @@ Deface::Override.new(:virtual_path => 'spree/products/_cart_form',
                      :name => 'use_drop_down_for_product_variants',
                      :replace_contents => '[id="product-variants"]',
                      :text => '
-                                  <h3 class="product-section-title"><%= Spree.t(:variants) %></h3>
+                              <h3 class="product-section-title"><%= Spree.t(:variants) %></h3>
+                              <div class="product-select-container">
+                              <label class="select-label">
                               <%= select_tag "variant_id",
     options_for_select(@product.variants_and_option_values(current_currency).collect{ |v| ["#{variant_options(v)}  #{variant_price(v)}", v.id] }), class: "property-widget-margin-top"%>
+    </label>
+  </div>
 ');
