d3dc0c139a7066495b741dea3147f3bf4002cdae
jgu <jgu@jeffreygu.com>
2016-02-14 12:01:51 -0600

    remove s3 from deev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,14 +47,4 @@ Rails.application.configure do
   config.logger.level = Logger.const_get(
     ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
   )
-
-  # Paperclip on AWS
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
 end
