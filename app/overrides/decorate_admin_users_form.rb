4279b82a61a56fe59d931cb3f4b5b8e65b0d1ba5
jgu <jgu@jeffreygu.com>
2016-03-02 18:59:12 -0700

    limited store view for non super_admins

--- app/overrides/decorate_admin_users_form.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(
-  virtual_path: "spree/admin/users/_form",
-  name: "multi_domain_admin_users_form_stores_dropdown",
-  insert_bottom: "[data-hook='admin_user_form_stock_locations']",
-  disabled: false,
-  partial: "spree/admin/shared/stores_dropdown"
-)
