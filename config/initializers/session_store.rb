d82ddf6fa1c3563a83aee8f494006685524b65af
jgu <jgu@jeffreygu.com>
2016-03-08 19:11:43 -0700

    hail mary to preserve user session on production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_callisto_session'
+#ensures that current_user is preserved across subdomains
+Rails.application.config.session_store :cookie_store, key: '_callisto_session', domain: :all, tld_length: 2
